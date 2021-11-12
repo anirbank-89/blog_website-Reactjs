75e3be372655b0ab5f660ca6ae5c0611a6a82c1f
Anirban Karmakar <karmakar_anirban89@outlook.com>
2021-11-12 23:28:29 +0530

    View change for one post in home page

--- src/components/home/Posts.jsx
+++ src/components/home/Posts.jsx
@@ -8,7 +8,7 @@ import OnePost from "./OnePost";
 const Posts = () => {
     var posts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
     return (
-        posts.map(post => (
+        posts.map(item => (
             <Grid item lg={3} sm={4} xs={12}>
                 <Link to={"/blog"} style={{textDecoration: 'none', color: 'inherit'}}>
                     <OnePost />
